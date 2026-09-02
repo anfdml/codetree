653c2d051b0c8b22fb056b07f73d329672c50809
junesung <anfdmlehwjr@gmail.com>
2026-09-02 22:52:54 +0900

    [Passed] 연속되는 수 4 (185ms, 13MB)

---
+++ trail2/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int count =1; 
+        int max = 1 ;
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i-1]<arr[i]) {
+                count++;
+                if(max<count) {
+                    max = count;
+                }
+            }else {
+                if(max<count) {
+                    max = count;
+                }
+                count = 1;
+            }
+        }
+        System.out.println(max);
+    }
+}
