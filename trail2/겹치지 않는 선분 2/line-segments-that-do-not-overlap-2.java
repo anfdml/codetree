0c1129622c12d14a13261c34f57014c9997ae8d8
junesung <anfdmlehwjr@gmail.com>
2026-09-14 00:58:36 +0900

    [Passed] 겹치지 않는 선분 2 (152ms, 10MB)

---
+++ trail2/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x1 = new int[n];
+        int[] x2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            boolean isok = false;
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                if((x1[i]>x1[j] && x2[i]<x2[j]) || x1[i]<x1[j] && x2[i]>x2[j]){
+                    isok = true;
+                }
+            }
+            if(!isok){
+                count++;
+            }
+
+
+
+        }
+        System.out.print(count);
+    }
+}
