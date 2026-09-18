a01245e5c1a584b6844b12a5eecd384fa9594d70
junesung <anfdmlehwjr@gmail.com>
2026-09-19 02:21:52 +0900

    [Passed] 가장 작은 x 찾기 (132ms, 10MB)

---
+++ trail2/가장 작은 x 찾기/find-smallest-x.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i <= 10000; i++) {
+            boolean isok = false;
+            for (int j = 0; j < n; j++) {
+                if (i * Math.pow(2, j + 1) >= a[j] && i * Math.pow(2, j + 1) <= b[j]) {
+                    isok = true;
+                } else {
+                    isok = false;
+                    break;
+                }
+            }
+            if (isok) min = Math.min(min, i);
+        }
+        System.out.print(min);
+    }
+}
