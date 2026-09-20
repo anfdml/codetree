eff4a67ad612c64b55aaa7e126d52ec037076ede
junesung <anfdmlehwjr@gmail.com>
2026-09-20 17:24:35 +0900

    [Passed] 수들의 최대 차 (229ms, 14MB)

---
+++ trail2/수들의 최대 차/maximum-difference-in-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] <= arr[j] && arr[i] + k >= arr[j]) {
+                    count++;
+                }
+            }
+            max = Math.max(max, count);
+        }
+        System.out.print(max);
+    }
+}
